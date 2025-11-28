3291e6db13503eb00d1b47c41496a124d3f5a9f0
Nicholas Bollweg <nick.bollweg@gmail.com>
2025-11-28 08:40:56 -0600

    more test work

--- recipe/run_rdflib_suite.py
+++ recipe/run_rdflib_suite.py
@@ -2,11 +2,10 @@ import sys
 from subprocess import call
 
 FAIL_UNDER = 81
-COV = ["coverage"]
-RUN = ["run", "--source=rdflib", "--branch", "-m"]
-PYTEST = ["pytest", "-vv", "--color=yes", "--tb=long"]
-REPORT = ["report", "--show-missing", "--skip-covered", f"--fail-under={FAIL_UNDER}"]
-
+SKIP_MARKERS = [
+    # https://github.com/conda-forge/rdflib-feedstock/pull/42   flakes out most days
+    "webtest",
+]
 SKIPS = [
     # https://github.com/conda-forge/rdflib-feedstock/pull/30   dep issues
     "definednamespace_creator",
@@ -14,20 +13,32 @@ SKIPS = [
     "berkeleydb",
     # https://github.com/conda-forge/rdflib-feedstock/pull/35   runtime pip install issues
     "test_plugins and (test_sparqleval or test_parser)",
+    # https://github.com/conda-forge/rdflib-feedstock/pull/42   needs docker
+    "test_rdf4j",
 ]
+
+COV = ["coverage"]
+RUN = ["run", "--source=rdflib", "--branch", "-m"]
+PYTEST = ["pytest", "-vv", "--color=yes", "--tb=long"]
+REPORT = ["report", "--show-missing", "--skip-covered", f"--fail-under={FAIL_UNDER}"]
 
 SKIP_OR = " or ".join(SKIPS)
 K = ["-k", f"not ({SKIP_OR})"]
+
+if SKIP_MARKERS:
+    M_OR = " or ".join(SKIP_MARKERS) if len(SKIP_MARKERS) > 1 else SKIP_MARKERS[0]
+    M = ["-m", M_OR]
 
 
 def do(*args: str) -> int:
     print("\t".join(args), flush=True)
     return call(args, cwd="src")
 
+
 if __name__ == "__main__":
     sys.exit(
         # run the tests
-        do(*COV, *RUN, *PYTEST, *K, "test")
+        do(*COV, *RUN, *PYTEST, *K, *M, "test")
         # ... then maybe run coverage
         or do(*COV, *REPORT)
     )
